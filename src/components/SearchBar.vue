b8c0a3ab7f47cbbd5a6e0d85a364934999ae73b6
jonat <jonathan.jeding@gmail.com>
2021-07-05 13:37:22 +0200

    init

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent, onUpdated, ref } from 'vue'
+
+export default defineComponent({
+  name: 'SearchBar',
+  setup() {
+    const searchText = ref('')
+    onUpdated(() => {
+    
+    })
+    return { searchText }
+  },
+  methods: {}
+})
+</script>
+<template lang="pug">
+input(v-model='searchText')
+</template>
