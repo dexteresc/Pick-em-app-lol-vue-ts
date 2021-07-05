9220c9491b5a1a61e584607d2bef7d7add8c395c
jonat <jonathan.jeding@gmail.com>
2021-07-05 14:49:37 +0200

    Typescript configuration changes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 import ChampionGrid from './ChampionGrid.vue'
 import Bans from './Bans.vue'
 import Players from './Players.vue'
-import SearchBar from './SearchBar.vue'
 import Side from './Side.vue'
-export { ChampionGrid, Bans, Players, SearchBar, Side }
+export { ChampionGrid, Bans, Players, Side }
