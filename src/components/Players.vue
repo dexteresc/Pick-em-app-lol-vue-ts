5d0b0a92ecdf6f2faeb3b2ae56048467e3a5b1ba
jonat <jonathan.jeding@gmail.com>
2021-07-07 14:47:55 +0200

    Added injection Added activate() to handle selecting a Player in relation to active type

--- src/components/Players.vue
+++ src/components/Players.vue
@@ -1,24 +1,61 @@
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { defineComponent, inject, PropType } from 'vue'
 import { Champion, Player } from '../App.vue'
 
 export default defineComponent({
   name: 'Players',
-  setup() {},
-  inject: ['currentVersion'],
+  setup() {
+    const active = inject('active')
+    const updateActive: any = inject('updateActive')
+    return { active, updateActive }
+  },
   props: {
-    players: Array as PropType<Player[]>
+    players: Array as PropType<Player[]>,
+    currentVersion: String
+  },
+  methods: {
+    activate(player: Player) {
+      if (this.active) {
+        if (this.typeGuard(this.active)) {
+          // If active is of Player type
+          console.log("is player")
+
+          this.updateActive(player)
+        } else {
+          // If active is not of Player type
+          console.log("is not player")
+        }
+      } else {
+        this.updateActive(player)
+      }
+    },
+    /**
+     * Check if object is of Player type
+     * @param {any} object Any object
+     * @returns {object} Champion object 
+     */
+    typeGuard(object: any): object is Player {
+      return 'champion' in object
+    }
   }
 })
 </script>
 <template lang="pug">
 div 
-  .player(v-for='player in players') 
-    .champion-img
-      img(
-        :src='player.champion"https://ddragon.leagueoflegends.com/cdn/" + currentVersion + "/img/champion/" + player.image'
-      )
+  .player(
+    v-for='player in players',
+    @click='activate(player)',
+    :class='active === player ? "active" : ""'
+  ) 
+    .champion-img {{ currentVersion }}
     .player-name {{ player.name }}
 </template>
 <style lang="scss" scoped>
+@import '../styles/_globals.scss';
+.champion-img {
+  & > div {
+    background-color: $primary-light;
+    outline: $light solid 3px;
+  }
+}
 </style>
